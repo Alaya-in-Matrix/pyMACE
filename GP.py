217a250f151665cbba7aaca831fad0830e88ae26
alaya <lvwenlong_lambda@qq.com>
2018-05-29 11:09:11 +0800

    GP tested

--- GP.py
+++ GP.py
@@ -15,9 +15,18 @@ class GP_MCMC:
         kern           = GPy.kern.Matern52(input_dim = self.dim, ARD = True)
         self.m         = GPy.models.GPRegression(self.train_x, self.train_y, kern)
 
-        self.m.kern.variance.set_prior(GPy.priors.Gamma.from_EV(np.var(self.train_y), 10.))
-        self.m.likelihood.variance.set_prior(GPy.priors.Gamma.from_EV(1e-2 * np.var(self.train_y), 10))
-        self.m.kern.lengthscale.set_prior(GPy.priors.Gamma.from_EV(np.std(self.train_x), 10 * np.ones(np.std(self.train_x).shape)))
+        # self.m.kern.variance.set_prior(GPy.priors.Gamma.from_EV(np.var(self.train_y), 120))
+        # self.m.likelihood.variance.set_prior(GPy.priors.Gamma.from_EV(1e-2 * np.var(self.train_y), 4))
+        # self.m.kern.lengthscale.set_prior(GPy.priors.Gamma.from_EV(np.std(self.train_x, 0), 1000 * np.ones(np.std(self.train_x, 0).shape)))
+        
+        self.m.kern.variance       = np.var(self.train_y)
+        self.m.kern.lengthscale    = np.std(self.train_x, 0)
+        self.m.likelihood.variance = 1e-2 * np.var(self.train_y)
+
+        # self.m.kern.variance.set_prior(GPy.priors.Gamma.from_EV(2, 4))
+        self.m.kern.variance.set_prior(GPy.priors.Gamma.from_EV(np.var(self.train_y), 120))
+        self.m.likelihood.variance.set_prior(GPy.priors.Gamma.from_EV(2, 4))
+        self.m.kern.lengthscale.set_prior(GPy.priors.Gamma.from_EV(2, 4))
 
         self.eps     = 1e-3;
         self.upsilon = 0.5;
@@ -27,46 +36,64 @@ class GP_MCMC:
         self.sample()
         
     def sample(self):
+        self.m.optimize(max_iters=100, messages=True)
         hmc    = GPy.inference.mcmc.HMC(self.m,stepsize=5e-2)
-        s      = hmc.sample(num_samples=100) # Burnin
-        s      = hmc.sample(num_samples=100)
-        self.s = s.copy()
-
-    def predict(self, x, hyp_vec):
-        self.m.kern[:] = hyp_vec
-        py, ps2        = self.m.predict(x)
-        return py, ps2;
+        s      = hmc.sample(num_samples=200) # Burnin
+        s      = hmc.sample(num_samples=50)
+        self.s = s
 
     def set_kappa(self):
         t = 1 + int(self.num_train / self.B)
         self.kappa = sqrt(self.upsilon * 2 * log(pow(t, 2.0 + self.dim / 2.0) * 3 * pow(np.pi, 2) / (3 * self.delta)));
 
-    def LCB(self, x):
+    def predict(self, x, hyp_vec):
+        self.m.kern.variance       = hyp_vec[0]
+        self.m.kern.lengthscale    = hyp_vec[1:1+self.dim]
+        self.m.likelihood.variance = hyp_vec[1+self.dim]
+        py, ps2                    = self.m.predict(x.reshape(x.size, 1))
+        return py, ps2;
+
+    def predict_all(self, x):
+        num_samples = self.s.shape[0]
+        pys         = np.zeros((num_samples, 1));
+        pss         = np.zeros((num_samples, 1));
+        for i in range(num_samples):
+            hyp     = self.s[i]
+            py, ps2 = self.predict(x, hyp)
+            pys[i]  = py[0][0]
+            pss[i]  = ps2[0][0]
+        return pys, np.sqrt(pss)
+
+    def LCB(self, x, pys, pss):
+        num_samples = pys.shape[0]
         self.set_kappa()
         acq = 0;
-        for hyp in self.s:
-            y, s = self.predict(x, hyp)
-            lcb  = y - self.kappa * np.sqrt(s)
+        for i in range(num_samples):
+            y    = pys[i]
+            s    = pss[i]
+            lcb  = y - self.kappa * s
             acq += lcb
         acq /= self.s.shape[0]
         return acq
 
-    def EI(self, x):
-        self.set_kappa()
+    def EI(self, x, pys, pss):
+        num_samples = pys.shape[0]
         acq = 0;
-        for hyp in self.s:
-            y, s         = self.predict(x, hyp)
+        for i in range(num_samples):
+            y            = pys[i]
+            s            = pss[i]
             phi, Phi, u  = get_quantiles(self.eps, self.tau, y, s)
             f_acqu       = s * (u * Phi + phi)
             acq         += f_acqu
         acq /= self.s.shape[0]
         return acq
 
-    def PI(self):
-        self.set_kappa()
+    def PI(self, x, pys, pss):
+        num_samples = pys.shape[0]
         acq = 0;
-        for hyp in self.s:
-            y, s         = self.predict(x, hyp)
+        for i in range(num_samples):
+            y            = pys[i]
+            s            = pss[i]
             phi, Phi, u  = get_quantiles(self.eps, self.tau, y, s)
             f_acqu       = Phi
             acq         += f_acqu
@@ -74,7 +101,8 @@ class GP_MCMC:
         return acq
 
     def MACE_acq(self, x):
-        lcb = self.LCB(x)
-        ei  = self.ei(x)
-        pi  = self.pi(x)
+        pys, pss = self.predict_all(x);
+        lcb      = self.LCB(x, pys, pss)
+        ei       = self.EI(x, pys, pss)
+        pi       = self.PI(x, pys, pss)
         return lcb, ei, pi
--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 import GPy
 import numpy as np
 import matplotlib.pyplot as plt
+from GP import GP_MCMC
 
-X    = np.random.uniform(-3, 3, (20, 2))
-Y    = 100 * np.sin(X[:, 0] + X[:, 1]) + np.random.randn(20,1)*0.05
-kern = GPy.kern.Matern52(input_dim = X.shape[1], variance = np.var(Y), ARD = True, lengthscale = np.std(X, 0))
-m    = GPy.models.GPRegression(X, Y, kern)
-m.likelihood.variance = 1e-2 * np.var(Y)
-
-m.kern.variance.set_prior(GPy.priors.Gamma.from_EV(np.var(Y), 10.))
-m.likelihood.variance.set_prior(GPy.priors.Gamma.from_EV(1e-2 * np.var(Y), 10.))
-m.kern.lengthscale.set_prior(GPy.priors.Gamma.from_EV(np.std(X), 10 * np.ones(np.std(X).shape)))
-
-# print(m)
-# print(m.kern)
-# print(m.likelihood)
-
-hmc = GPy.inference.mcmc.HMC(m,stepsize=5e-2)
-s   = hmc.sample(num_samples=50) # Burnin
-s   = hmc.sample(num_samples=50)
-print(s)
-
-# print(m.kern.lengthscale)
-# m.optimize_restarts(num_restarts = 10)
-# print(m.kern.lengthscale)
-# fig = m.plot()
-# GPy.plotting.show(fig, filename='basic_gp_regression_notebook')
-
+num_train = 20;
+train_x   = np.random.uniform(-3, 3, (num_train, 1))
+train_y   = 100 * np.sin(train_x).reshape(num_train, 1) + np.random.randn(num_train,1)*0.05
+gp        = GP_MCMC(train_x, train_y, 4);
